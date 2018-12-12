ac752cddd2a8bf0b7b52521103f190ac776414c5
christanismerilbanzouzi <merilb78@gmail.com>
2018-12-12 14:47:57 +0100

    first commit

---
+++ app/src/main/java/com/resocoder/forecastmvvm/ForecastApplication.kt
@@ -0,0 +1,41 @@
+package com.resocoder.forecastmvvm
+
+import android.app.Application
+import android.preference.PreferenceManager
+import com.jakewharton.threetenabp.AndroidThreeTen
+import com.resocoder.forecastmvvm.data.db.ForecastDatabase
+import com.resocoder.forecastmvvm.data.network.*
+import com.resocoder.forecastmvvm.data.provider.UnitProvider
+import com.resocoder.forecastmvvm.data.provider.UnitProviderImpl
+import com.resocoder.forecastmvvm.data.repository.ForecastRepository
+import com.resocoder.forecastmvvm.data.repository.ForecastRepositoryImpl
+import com.resocoder.forecastmvvm.ui.weather.current.CurrentWeatherViewModelFactory
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
+import org.kodein.di.android.x.androidXModule
+import org.kodein.di.generic.bind
+import org.kodein.di.generic.instance
+import org.kodein.di.generic.provider
+import org.kodein.di.generic.singleton
+
+
+class ForecastApplication : Application(), KodeinAware {
+    override val kodein = Kodein.lazy {
+        import(androidXModule(this@ForecastApplication))
+
+        bind() from singleton { ForecastDatabase(instance()) }
+        bind() from singleton { instance<ForecastDatabase>().currentWeatherDao() }
+        bind<ConnectivityInterceptor>() with singleton { ConnectivityInterceptorImpl(instance()) }
+        bind() from singleton { ApixuWeatherApiService(instance()) }
+        bind<WeatherNetworkDataSource>() with singleton { WeatherNetworkDataSourceImpl(instance()) }
+        bind<ForecastRepository>() with singleton { ForecastRepositoryImpl(instance(), instance()) }
+        bind<UnitProvider>() with singleton { UnitProviderImpl(instance()) }
+        bind() from provider { CurrentWeatherViewModelFactory(instance(), instance()) }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        AndroidThreeTen.init(this)
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false)
+    }
+}
---
+++ app/src/main/java/com/resocoder/forecastmvvm/ui/weather/current/CurrentWeatherViewModel.kt
@@ -0,0 +1,21 @@
+package com.resocoder.forecastmvvm.ui.weather.current
+
+import androidx.lifecycle.ViewModel;
+import com.resocoder.forecastmvvm.data.provider.UnitProvider
+import com.resocoder.forecastmvvm.data.repository.ForecastRepository
+import com.resocoder.forecastmvvm.internal.UnitSystem
+import com.resocoder.forecastmvvm.internal.lazyDeferred
+
+class CurrentWeatherViewModel(
+    private val forecastRepository: ForecastRepository,
+    unitProvider: UnitProvider
+) : ViewModel() {
+    private val unitSystem = unitProvider.getUnitSystem()
+
+    val isMetric: Boolean
+        get() = unitSystem == UnitSystem.METRIC
+
+    val weather by lazyDeferred {
+        forecastRepository.getCurrentWeather(isMetric)
+    }
+}
